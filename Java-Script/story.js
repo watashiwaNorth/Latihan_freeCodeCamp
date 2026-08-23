92314f1ce137e299c98ab1466cb166aecd5bfdac
watashiwaNorth <mrfirdausramdan@gmail.com>
2026-08-23 22:49:19 +0800

    #

---
+++ Java-Script/story.js
@@ -0,0 +1,20 @@
+let adjective = "love";
+let noun = "book";
+let verb = "reading";
+let place = "library";
+let adjective2 ="hate";
+let noun2 = "handphone";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + [noun] + " lived in a " + [place] + " and had " + [adjective2] + " nostrils that blew fire when it was " + [verb] + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "fresh";
+noun = "fish";
+verb = "eating";
+place = "campfire";
+adjective2 ="so good";
+noun2 = "potato";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + [noun] + " lived in a " + [place] + " and had " + [adjective2] + " nostrils that blew fire when it was " + [verb] + "."
+console.log("Second story: " + secondStory);
